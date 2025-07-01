79583af9436016f386902d95e972029c44047cf1
cocoheart0128 <130530818+cocoheart0128@users.noreply.github.com>
2025-07-01 15:23:50 +0900

    Update make_sample.py

--- aws-glue-monitoring-app/app/make_sample.py
+++ aws-glue-monitoring-app/app/make_sample.py
@@ -123,3 +123,140 @@ def make_job_sample():
       ]
   }
   return sample_job_runs
+
+
+
+def make_crawler_sample():
+    sample_crawler_runs = {
+        "crawler1": [
+            {
+                'CrawlerMetricsList': [
+                    {
+                        'CrawlerName': 'crawler1',
+                        'TimeLeftSeconds': 0.0,
+                        'StillEstimating': False,
+                        'LastRuntimeSeconds': 69.688,
+                        'MedianRuntimeSeconds': 60.034,
+                        'TablesCreated': 5,
+                        'TablesUpdated': 3,
+                        'TablesDeleted': 0
+                    }
+                ],
+                'ResponseMetadata': {
+                    'RequestId': 'c527',
+                    'HTTPStatusCode': 200,
+                    'HTTPHeaders': {
+                        'date': 'Tue, 01 Jul 2025 05:41:21 GMT',
+                        'content-type': 'application/x-amz-json-1.1',
+                        'content-length': '240',
+                        'connection': 'keep-alive',
+                        'x-amzn-requestid': 'c527',
+                        'cache-control': 'no-cache'
+                    },
+                    'RetryAttempts': 0
+                }
+            },
+            {
+                'Name': 'crawler1',
+                'Role': 'crawler-role',
+                'Targets': {
+                    'S3Targets': [
+                        {'Path': 's3://bucket/folder/', 'Exclusions': []}
+                    ],
+                    'JdbcTargets': [],
+                    'MongoDBTargets': [],
+                    'DynamoDBTargets': [],
+                    'CatalogTargets': [],
+                    'DeltaTargets': [],
+                    'IcebergTargets': [],
+                    'HudiTargets': []
+                },
+                'DatabaseName': 'test',
+                'Classifiers': [],
+                'RecrawlPolicy': {'RecrawlBehavior': 'CRAWL_NEW_FOLDERS_ONLY'},
+                'SchemaChangePolicy': {'UpdateBehavior': 'LOG', 'DeleteBehavior': 'LOG'},
+                'LineageConfiguration': {'CrawlerLineageSettings': 'DISABLE'},
+                'State': 'READY',
+                'TablePrefix': 'crt_stock_',
+                'CrawlElapsedTime': 0,
+                'CreationTime': datetime(2023, 9, 25, 16, 57, 44, tzinfo=tzlocal()),
+                'LastUpdated': datetime(2025, 6, 17, 8, 18, tzinfo=tzlocal()),
+                'LastCrawl': {
+                    'Status': 'SUCCEEDED',
+                    'LogGroup': '/aws-glue/crawlers',
+                    'LogStream': 'crawler1',
+                    'MessagePrefix': '164a',
+                    'StartTime': datetime(2025, 7, 1, 1, 52, 46, tzinfo=tzlocal())
+                },
+                'Version': 4,
+                'Configuration': '{"Version":1.0,"CreatePartitionIndex":false}',
+                'LakeFormationConfiguration': {'UseLakeFormationCredentials': False, 'AccountId': ''}
+            }
+        ],
+        "crawler2": [
+            {
+                'CrawlerMetricsList': [
+                    {
+                        'CrawlerName': 'crawler2',
+                        'TimeLeftSeconds': 0.0,
+                        'StillEstimating': False,
+                        'LastRuntimeSeconds': 105.22,
+                        'MedianRuntimeSeconds': 98.43,
+                        'TablesCreated': 8,
+                        'TablesUpdated': 0,
+                        'TablesDeleted': 1
+                    }
+                ],
+                'ResponseMetadata': {
+                    'RequestId': 'd913',
+                    'HTTPStatusCode': 200,
+                    'HTTPHeaders': {
+                        'date': 'Tue, 01 Jul 2025 06:11:42 GMT',
+                        'content-type': 'application/x-amz-json-1.1',
+                        'content-length': '250',
+                        'connection': 'keep-alive',
+                        'x-amzn-requestid': 'd913',
+                        'cache-control': 'no-cache'
+                    },
+                    'RetryAttempts': 0
+                }
+            },
+            {
+                'Name': 'crawler2',
+                'Role': 'crawler-role',
+                'Targets': {
+                    'S3Targets': [
+                        {'Path': 's3://another-bucket/data/', 'Exclusions': ['*.tmp']}
+                    ],
+                    'JdbcTargets': [],
+                    'MongoDBTargets': [],
+                    'DynamoDBTargets': [],
+                    'CatalogTargets': [],
+                    'DeltaTargets': [],
+                    'IcebergTargets': [],
+                    'HudiTargets': []
+                },
+                'DatabaseName': 'analytics',
+                'Classifiers': [],
+                'RecrawlPolicy': {'RecrawlBehavior': 'CRAWL_EVERYTHING'},
+                'SchemaChangePolicy': {'UpdateBehavior': 'UPDATE_IN_DATABASE', 'DeleteBehavior': 'DELETE_FROM_DATABASE'},
+                'LineageConfiguration': {'CrawlerLineageSettings': 'ENABLE'},
+                'State': 'READY',
+                'TablePrefix': 'etl_',
+                'CrawlElapsedTime': 0,
+                'CreationTime': datetime(2024, 1, 10, 10, 0, 0, tzinfo=tzlocal()),
+                'LastUpdated': datetime(2025, 6, 30, 15, 0, tzinfo=tzlocal()),
+                'LastCrawl': {
+                    'Status': 'FAILED',
+                    'LogGroup': '/aws-glue/crawlers',
+                    'LogStream': 'crawler2',
+                    'MessagePrefix': '2fbc',
+                    'StartTime': datetime(2025, 7, 1, 2, 5, 30, tzinfo=tzlocal())
+                },
+                'Version': 7,
+                'Configuration': '{"Version":1.0,"CreatePartitionIndex":true}',
+                'LakeFormationConfiguration': {'UseLakeFormationCredentials': True, 'AccountId': '123456789012'}
+            }
+        ]
+    }
+    return sample_crawler_runs
